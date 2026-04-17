af7cf1965747bd064c782f8428294a03ba9370bb
Ifath Ahamed <ifathahamed01@gmail.com>
2026-04-18 00:49:45 +0530

    Change API base URL to production endpoint

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://ict-tutor-ai.onrender.com/";
 export const TOKEN_KEY = "ict_tutor_token";
 
 export const api = axios.create({
