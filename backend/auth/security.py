534883579a9afef619ff7d72cff3b917d3c833d6
Ifath Ahamed <ifathahamed01@gmail.com>
2026-04-18 15:39:19 +0530

    Update hash_password to handle byte encoding
    Ensure password safety by encoding and limiting length.

--- backend/auth/security.py
+++ backend/auth/security.py
@@ -11,7 +11,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
-    return pwd_context.hash(password[:72])
+    password_bytes = password.encode("utf-8")
+    safe_password = password_bytes[:72].decode("utf-8", errors="ignore")
+    return pwd_context.hash(safe_password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
